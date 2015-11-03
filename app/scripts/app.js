283d01b7971011f6492512b20762f8768891f29d
Ronny Hartenstein <ronny@rh-flow.de>
2015-11-03 22:20:11 +0100

    refactored

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,37 +12,52 @@
     // debug window
     require('nw.gui').Window.get().showDevTools();
 
-    var $step1 = $('.row.step1');
-    var $step2 = $('.row.step2');
-    var $step3 = $('.row.step3');
+    workflow_step1();
+    workflow_step2();
+    workflow_step3();
 
-    $('.step1 .weiter').on('click', function() {
-      // TODO muss wieder raus, umschalten automatisch wenn IP erreicht wird (und Sensor antwortet)
-      $step1.fadeOut(function() { $step2.fadeIn(); });
-    });
-    $('.step2 .weiter').on('click', function() {
-      // TODO WLAN-SSID+Pwd speichern zum Sensor
-      status('info','Übertrage Daten zum Sensor...');
-      window.setTimeout(function() {
-        status('success','Einstellungen wurden übertragen und gespeichert.');
-        $step2.fadeOut(function() { $step3.fadeIn(); });
-      }, 1000);
-
-    });
-    $('.step2 .zurueck').on('click', function() {
-      $step2.fadeOut(function() { $step1.fadeIn(); });
-    });
-    $('.step3 .speichern').on('click', function() {
-      // TODO Sensoren-Einstellungen speichern
-      status('info','Übertrage Daten zum Sensor...');
-      window.setTimeout(function() {
-        status('success','Einstellungen wurden übertragen und gespeichert.');
-      }, 1000);
-    });
-    $('.step3 .zurueck').on('click', function() {
-      $step3.fadeOut(function() { $step2.fadeIn(); });
-    });
 })();
+
+function workflow_step1() {
+  var $step1 = $('.row.step1');
+  var $step2 = $('.row.step2');
+  $('.step1 .weiter').on('click', function() {
+    // TODO muss wieder raus, umschalten automatisch wenn IP erreicht wird (und Sensor antwortet)
+    $step1.fadeOut(function() { $step2.fadeIn(); });
+  });
+}
+
+function workflow_step2() {
+  var $step1 = $('.row.step1');
+  var $step2 = $('.row.step2');
+  var $step3 = $('.row.step3');
+  $('.step2 .speichern').on('click', function() {
+    // TODO WLAN-SSID+Pwd speichern zum Sensor
+    status('info','Übertrage Daten zum Sensor...');
+    window.setTimeout(function() {
+      status('success','Einstellungen wurden übertragen und gespeichert.', false);
+      $step2.fadeOut(function() { $step3.fadeIn(); });
+    }, 1000);
+  });
+  $('.step2 .zurueck').on('click', function() {
+    $step2.fadeOut(function() { $step1.fadeIn(); });
+  });
+}
+
+function workflow_step3() {
+  var $step2 = $('.row.step2');
+  var $step3 = $('.row.step3');
+  $('.step3 .speichern').on('click', function() {
+    // TODO Sensoren-Einstellungen speichern
+    status('info','Übertrage Daten zum Sensor...');
+    window.setTimeout(function() {
+      status('success','Einstellungen wurden übertragen und gespeichert.', false);
+    }, 1000);
+  });
+  $('.step3 .zurueck').on('click', function() {
+    $step3.fadeOut(function() { $step2.fadeIn(); });
+  });
+}
 
 function ping_sensor(host) {
   console.log("ping sensor");
@@ -62,8 +77,11 @@ function start_ping() {
   ping_sensor(host);
 }
 
-function status(status, message) {
+function status(status, message, fadeout) {
+  fadeout = fadeout || true;
   $status = $('#alertbox');
   $status.show().html('<div class="alert alert-' + status + '" role="alert">' + message + '</div>');
-  window.setTimeout(function() { $status.fadeOut('slow'); }, 1000);
+  if (fadeout) window.setTimeout(function() {
+    $status.fadeOut('slow');
+  }, 2000);
 }
